823d2fbbd693b6eb009bb1d0a7d89a84acbbca6f
Bill Glick <bglick@illinois.edu>
2021-06-10 12:51:04 -0500

    Add backend, frontend, listen resources

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,8 +3,10 @@
 # @example
 #   include profile_haproxy
 class profile_haproxy (
+  $backend         = undef,
   $balancemember   = undef,
   $firewall        = undef,
+  $frontend        = undef,
   $listen          = undef,
   $sslcertificates = undef,
 ) {
@@ -17,48 +19,16 @@ class profile_haproxy (
 
   include ::haproxy
 
-#  if ( $balancemember ) {
-    ## Export a balancermember server, note that the listening_service parameter
-    ##  will/must correlate with an haproxy::listen defined resource type.
-    #@@haproxy::balancermember { $fqdn:
-    #  order                  => '21',
-    #  listening_service      => 'puppet00',
-    #  server_name            => $::hostname,
-    #  balancer_ip            => $::ipaddress,
-    #  balancer_port          => '8140',
-    #  balancermember_options => 'check',
-    #}
-#  }
+  if ( $frontend ) {
+    ensure_resources( 'haproxy::frontend', $frontend )
+  }
 
-#  if ( $listen ) {
-    ## Declare a couple of Listening Services for haproxy.cfg
-    ##  Note that the balancermember server resources are being collected in
-    ##  the haproxy::config defined resource type with the following line:
-    ##  Haproxy::Balancermember <<| listening_service == $name |>>
-#    ensure_resources( 'haproxy::listen', $listen )
-    #haproxy::listen { 'puppet00':
-    #  order     => '20',
-    #  ipaddress => $::ipaddress,
-    #  ports     => '18140',
-    #  options   => {
-    #    'option'  => [
-    #      'tcplog',
-    #    ],
-    #    'balance' => 'roundrobin',
-    #  },
-    #}
-    #haproxy::listen { 'stats':
-    #  order     => '30',
-    #  ipaddress => '',
-    #  ports     => '9090',
-    #  options   => {
-    #    'mode'  => 'http',
-    #    'stats' => [
-    #      'uri /',
-    #      'auth puppet:puppet',
-    #    ],
-    #  },
-    #}
-#  }
+  if ( $backend ) {
+    ensure_resources( 'haproxy::frontend', $frontend )
+  }
+
+  if ( $listen ) {
+    ensure_resources( 'haproxy::listen', $listen )
+  }
 
 }
